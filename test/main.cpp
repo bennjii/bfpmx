68b3131b2ff5daa69dbc30218fb6898e27b4f5ca
Ben White <contact@bennjii.dev>
2025-10-24 18:30:37 +0200

    fix(main): update comments to reflect current usage

--- test/main.cpp
+++ test/main.cpp
@@ -7,18 +7,17 @@
 #include "definition/prelude.h"
 
 int main() {
+    using FloatRepr = fp8::E4M3Type;
+
     // We are constructing a 4:3:1=8bit Float
-    std::cout << "FloatSize=" << fp8::E4M3Type::Size() << "bits" << std::endl;
+    std::cout << "FloatSize=" << FloatRepr::Size() << " bits" << std::endl;
 
-    // We expect 16 + 32(8) = 272 bits
-    using BlockT = Block<32, 4, fp8::E4M3Type, CPUArithmetic, SharedExponentQuantization>;
+    // We pick particular arithmetic and quantization policies
+    using BlockT = Block<32, 4, FloatRepr, CPUArithmetic, MaximumFractionalQuantization>;
 
-    // Below will be const-folded into Block<32, 272>::Length(), which statically
-    // is known as 32.
     const auto block1 = BlockT(std::to_array<f64, 4>({10, 15, 20, 25}));
     const auto block2 = BlockT(std::to_array<f64, 4>({2., 3., 4., 5.}));
 
     const auto block = block1 + block2;
-    std::cout << "BlockLength=" << block.Length() << " elements" << std::endl;
-    std::cout << "block: " << block.asString() << std::endl;
+    std::cout << "Block=" << block.asString() << std::endl;
 }
