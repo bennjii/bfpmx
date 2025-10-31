e78be20f0e18cc7542a31b810d3661b53fa53aea
Ben White <contact@bennjii.dev>
2025-10-31 12:44:21 +0100

    feat(block): block dimensional testing

---
+++ mx/definition/block_float/block/BlockDims.test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Benjamin White on 10/10/2025.
+//
+
+#include "BlockDims.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "definition/prelude.h"
+
+TEST_CASE("BlockDims Construction") {
+  REQUIRE(true);
+}
