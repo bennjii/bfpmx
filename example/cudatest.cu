7493d490133f83bf4320cd5e1fdf51f408a9bf7f
souhhcong <souhhcong@gmail.com>
2025-11-10 10:52:48 +0100

    fix(gpu): common BlockT alias for both CPU and GPU

--- example/cudatest.cu
+++ example/cudatest.cu
@@ -1,24 +1,29 @@
 #include <iostream>
 
 #include "definition/prelude.h"
+using FloatReprCUDATest = fp8::E4M3Type;
+template<template <typename T> typename ArithmeticPolicy>
+using BlockT = Block<32, BlockDims<4>, FloatReprCUDATest, ArithmeticPolicy, MaximumFractionalQuantization>;
+using BlockT_CPU = BlockT<CPUArithmetic>;
+using BlockT_GPU = BlockT<GPUArithmetic>;
 
 int main() {
-    using FloatRepr = fp8::E4M3Type;
-
+    std::array<f64, 4> testArray1 = {10, 15, 20, 25};
+    std::array<f64, 4> testArray2 = {2., 3., 4., 5.};
     // We are constructing a 4:3:1=8bit Float
-    std::cout << "FloatSize=" << FloatRepr::Size() << " bits" << std::endl;
+    std::cout << "FloatSize=" << FloatReprCUDATest::Size() << " bits" << std::endl;
 
     // We pick particular arithmetic and quantization policies
-    using BlockT_GPU = Block<32, BlockDims<4>, FloatRepr, GPUArithmetic, MaximumFractionalQuantization>;
-    const auto block1_GPU = BlockT_GPU(std::to_array<f64, 4>({10, 15, 20, 25}));
-    const auto block2_GPU = BlockT_GPU(std::to_array<f64, 4>({2., 3., 4., 5.}));
+    const auto block1_GPU = BlockT_GPU(testArray1);
+    const auto block2_GPU = BlockT_GPU(testArray2);
     const auto block_GPU = block1_GPU + block2_GPU;
-    using BlockT_CPU = Block<32, BlockDims<4>, FloatRepr, CPUArithmetic, MaximumFractionalQuantization>;
-    const auto block1_CPU = BlockT_CPU(std::to_array<f64, 4>({10, 15, 20, 25}));
-    const auto block2_CPU = BlockT_CPU(std::to_array<f64, 4>({2., 3., 4., 5.}));
+
+    const auto block1_CPU = BlockT_CPU(testArray1);
+    const auto block2_CPU = BlockT_CPU(testArray2);
     const auto block_CPU = block1_CPU + block2_CPU;
     std::cout << "BlockGPU=" << block_GPU.asString() << std::endl;
     std::cout << "BlockCPU=" << block_CPU.asString() << std::endl;
+    // Only returns correct value when run on devices with a NVIDIA GPU
     if (block_GPU.Spread() == block_CPU.Spread()) {
         std::cout << "GPU and CPU results match!" << std::endl;
     } else {
