e7c11a1d5e3e6d507609a839414b5dc957a7bf4d
gabriele <gabriele.baietta03@gmail.com>
2025-11-28 10:14:34 +0100

    fixing jacobi so that compiler cannot optimize everything at comp-time

--- benchmarks/jacobi.cpp
+++ benchmarks/jacobi.cpp
@@ -22,13 +22,12 @@ using TestingBlock = Block<TestingScalarSize, Dimensions, TestingFloat,
 template <size_t N>
 static void jacobi_2d_array(
   const int steps,
-  std::array<std::array<f64, N>, N> A,
-  std::array<std::array<f64, N>, N> B
+  std::array<std::array<f64, N>, N> &A,
+  std::array<std::array<f64, N>, N> &B
 ) {
   profiler::func();
   int t, i, j;
 
-#pragma scop
   for (t = 0; t < steps; t++)
   {
     for (i = 1; i < N - 1; i++)
@@ -38,14 +37,13 @@ static void jacobi_2d_array(
       for (j = 1; j < N - 1; j++)
         A[i][j] = 0.2f * (B[i][j] + B[i][j-1] + B[i][1+j] + B[1+i][j] + B[i-1][j]);
   }
-#pragma endscop
 }
 
 template <size_t N>
 static void jacobi_2d_block(
   const int steps,
-  TestingBlock<BlockDims<N, N>> A,
-  TestingBlock<BlockDims<N, N>> B
+  TestingBlock<BlockDims<N, N>> &A,
+  TestingBlock<BlockDims<N, N>> &B
 ) {
   profiler::func();
 
@@ -75,8 +73,8 @@ static void jacobi_2d_block(
 template <size_t N>
 static void jacobi_2d_block_wo_marsh(
   const int steps,
-  TestingBlock<BlockDims<N, N>> A,
-  TestingBlock<BlockDims<N, N>> B
+  TestingBlock<BlockDims<N, N>> &A,
+  TestingBlock<BlockDims<N, N>> &B
 ) {
   profiler::func();
 
@@ -88,9 +86,13 @@ static void jacobi_2d_block_wo_marsh(
   // TODO: block mulAt by scalar
   const auto scalar = TestingBlock<BlockDims<1>>(std::array<f64, 1>{0.2f});
   const auto sBias = scalar.ScalarBits();
+
+  using TypeA = std::remove_reference_t<decltype(A)>;
+  using TypeS = decltype(scalar);
+
   #define AT(i,j)  ((i)*N + (j))
-  #define ADD(...) CPUArithmeticSingularValues<decltype(B), decltype(A), decltype(A)>::AddAt(__VA_ARGS__);
-  #define MUL(...) CPUArithmeticSingularValues<decltype(B), decltype(B), decltype(scalar)>::MulAt(__VA_ARGS__);
+  #define ADD(...) CPUArithmeticSingularValues<TypeA, TypeA, TypeA>::AddAt(__VA_ARGS__);
+  #define MUL(...) CPUArithmeticSingularValues<TypeA, TypeA, TypeS>::MulAt(__VA_ARGS__);
 
   for (u32 t = 0; t < steps; t++) {
     for (u32 i = 1; i < N - 1; i++) {
@@ -115,22 +117,24 @@ static void jacobi_2d_block_wo_marsh(
   }
 }
 
-constexpr u32 N = 32;
+constexpr u32 N = 128;
 
-int main() {
-  profiler::begin();
-
+int main(int argc, char **argv) {
   using Size = BlockDims<N, N>;
   using Block = TestingBlock<Size>;
 
-  const auto a = std::array<std::array<f64, N>, N>{
-    std::array<f64, N>{1.2f},
-    std::array<f64, N>{3.4f},
+  // TODO: unsafe
+  double x1 = argc > 1 ? std::stod(argv[1]): 1.2f;
+  double x2 = argc > 2 ? std::stod(argv[1]): 3.4f;
+
+  auto a = std::array<std::array<f64, N>, N>{
+    std::array<f64, N>{x1},
+    std::array<f64, N>{x2},
   };
 
-  const auto b = std::array<std::array<f64, N>, N>{
-    std::array<f64, N>{1.2f},
-    std::array<f64, N>{3.4f},
+  auto b = std::array<std::array<f64, N>, N>{
+    std::array<f64, N>{x1},
+    std::array<f64, N>{x2},
   };
 
   std::array<f64, N * N> aLinear = {};
@@ -147,16 +151,34 @@ int main() {
     }
   }
 
-  const auto blockA = Block(aLinear);
-  const auto blockB = Block(bLinear);
+  auto dummy_sum_double = [&] (auto &a, auto &b) -> double {
+      double sum = 0;
+      for (auto &x: a) for (auto &y: x) sum += y;
+      for (auto &x: b) for (auto &y: x) sum += y;
+      return sum;
+  };
 
-  jacobi_2d_array<N>(250, a, b);
-  jacobi_2d_block<N>(250, blockA, blockB);
-  jacobi_2d_block_wo_marsh<N>(250, blockA, blockB);
+  auto dummy_sum_block = [&] (auto &a, auto &b) -> double {
+      double sum = 0;
+    for (u32 i = 1; i < N - 1; i++) 
+      for (u32 j = 1; j < N - 1; j++) 
+          sum += a[i, j] + b[i, j];
+    return sum;
+  };
 
-  // TODO: check results
-
+  // TODO: check results instead of using `dummy_sum`
+  int steps = 250;
+  profiler::begin();
+  jacobi_2d_array<N>(steps, a, b);
+  std::cout << dummy_sum_double(a, b) << " ";
+  auto blockA = Block(aLinear), blockB = Block(bLinear);
+  jacobi_2d_block<N>(steps, blockA, blockB);
+  std::cout << dummy_sum_block(blockA, blockB) << " ";
+  blockA = Block(aLinear), blockB = Block(bLinear);
+  jacobi_2d_block_wo_marsh<N>(steps, blockA, blockB);
+  std::cout << dummy_sum_block(blockA, blockB) << "\n";
   profiler::end_and_print();
+
   return 0;
 }
 
